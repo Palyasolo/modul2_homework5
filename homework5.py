9a9c3566db1d2fccabf3a9127a0bb03cc6e004fc
Palyasolo <soloduhin.pavel@ya.ru>
2024-07-21 20:49:12 +0700

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,12 @@
+def get_matrix(n, m, volue):
+    matrix = []
+    for i in range (n):
+        a = []
+        matrix.append(a)
+        for j in range (m):
+            a.append(volue)
+    print(matrix)
+    return matrix
+
+
+get_matrix(1, 3, 7)
